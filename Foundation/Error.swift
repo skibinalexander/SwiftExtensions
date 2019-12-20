efb9200d946a5f7f659137d80c9dd714f775a948
skibinalexander <skibinalexander@gmail.com>
2019-12-20 18:13:20 +0300

    - update extension

--- Foundation/Error.swift
+++ Foundation/Error.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 extension Error {
-    var code: Int           { return (self as NSError).code }
-    var domain: String      { return (self as NSError).domain }
+    var code: Int { return (self as NSError).code }
+    var domain: String { return (self as NSError).domain }
+    var userInfo: [String : Any] { return (self as NSError).userInfo }
 }
